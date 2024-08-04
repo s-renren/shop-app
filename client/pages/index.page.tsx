56d41f612c9e8dd42d46d59fe710358088a69a1e
s-renren <s1f102301156@iniad.org>
2024-08-04 16:14:06 +0900

    検索ボックスを作成

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -1,7 +1,14 @@
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container}>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <div>欲しいものを検索</div>
+        <input placeholder="例:マグカップ"></input>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
