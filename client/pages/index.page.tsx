9927fd6b30bc9237ac97f7dbc0f684fd1b0d51ac
s-renren <s1f102301156@iniad.org>
2024-08-05 15:31:24 +0900

    欲しいものリストを入力したら表示するようにした

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [wishList, setWishList] = useState<string>('');
+  const [wishListItems, setWishListItems] = useState<string[]>([]);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWishList(event.target.value);
+  };
+  const addWishListButton = () => {
+    if (wishList.trim() !== '') {
+      setWishListItems([...wishListItems, wishList]);
+      setWishList('');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div>
-        <div>欲しいものを検索</div>
-        <input placeholder="例:マグカップ"></input>
+        <input placeholder="+欲しいものリスト" value={wishList} onChange={handleInputChange} />
+        <button onClick={addWishListButton}>addWishList</button>
+        <div>
+          {wishListItems.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </div>
       </div>
     </div>
   );
