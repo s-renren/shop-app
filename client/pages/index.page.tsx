29ecf40d4c534e244409c103c037bc4099b65eeb
s-renren <s1f102301156@iniad.org>
2024-08-05 18:15:56 +0900

    追加したものを一覧表示できるようにした

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -4,6 +4,8 @@ import styles from './index.module.css';
 const Home = () => {
   const [wishList, setWishList] = useState<string>('');
   const [wishListItems, setWishListItems] = useState<string[]>([]);
+  const [wishListOpen, setWishListOpen] = useState<boolean>(false);
+
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setWishList(event.target.value);
   };
@@ -13,17 +15,32 @@ const Home = () => {
       setWishList('');
     }
   };
+  const ClickMakeList = () => {
+    if (wishListOpen === true) {
+      setWishListOpen(false);
+    } else setWishListOpen(true);
+  };
 
   return (
     <div className={styles.container}>
-      <div>
-        <input placeholder="+欲しいものリスト" value={wishList} onChange={handleInputChange} />
-        <button onClick={addWishListButton}>addWishList</button>
+      <div onClick={ClickMakeList} className={styles.mainpageFunctions}>＋リストを作成する</div>
+      <div className={!wishListOpen ? styles.wishListClose : styles.wishListOpen}>
         <div>
+          <input
+            placeholder="+欲しいものリスト"
+            value={wishList}
+            onChange={handleInputChange}
+            className={styles.inputWishList}
+          />
+          <button onClick={addWishListButton} className={styles.buttonWishList}>
+            addWishList
+          </button>
+        </div>
+        <h3>
           {wishListItems.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
-        </div>
+        </h3>
       </div>
     </div>
   );
